11566d9598ebb6adb518d2e929b88e71503bc971
Danil Gulin <d.nk.gulin@gmail.com>
2025-08-06 15:51:15 +0200

    Add Span extensions

---
+++ Runtime/RefListSpanExtensions.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace DnDev
+{
+    public static class RefListSpanExtensions
+    {
+        public static ReadOnlySpan<T> AsReadonlySpan<T>(this in RefList<T> self) where T : struct
+        {
+            return new ReadOnlySpan<T>(self.Buffer, 0, self.Count);
+        }
+
+        public static unsafe ReadOnlySpan<T> AsReadonlySpan<T>(this in TempRefList<T> self) where T : unmanaged
+        {
+            return new ReadOnlySpan<T>(self.Buffer, self.Count);
+        }
+
+        public static unsafe ReadOnlySpan<T> AsReadonlySpan<T>(this in NativeRefList<T> self) where T : unmanaged
+        {
+            return new ReadOnlySpan<T>(self.Buffer, self.Count);
+        }
+
+        public static Span<T> AsSpan<T>(this ref RefList<T> self) where T : struct
+        {
+            return new Span<T>(self.Buffer, 0, self.Count);
+        }
+
+        public static unsafe Span<T> AsSpan<T>(this ref TempRefList<T> self) where T : unmanaged
+        {
+            return new Span<T>(self.Buffer, self.Count);
+        }
+
+        public static unsafe Span<T> AsSpan<T>(this ref NativeRefList<T> self) where T : unmanaged
+        {
+            return new Span<T>(self.Buffer, self.Count);
+        }
+    }
+}
