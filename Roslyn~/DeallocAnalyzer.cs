75d698d67a847a58fdecc0a2e2a174e24f6b8468
Danil Gulin <d.nk.gulin@gmail.com>
2025-08-29 13:13:03 +0200

    Replcae code generated warning with a Roslyn one

--- Roslyn~/DeallocAnalyzer.cs
+++ Roslyn~/DeallocAnalyzer.cs
@@ -1,6 +1,8 @@
 using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace DnDev.Roslyn
@@ -10,25 +12,33 @@ namespace DnDev.Roslyn
     {
         private static int _ruleId;
 
-        private static DiagnosticDescriptor Rule(string title, string msg)
+        private static DiagnosticDescriptor Rule(string title, string msg, DiagnosticSeverity severity)
         {
             return new DiagnosticDescriptor(
-                id: $"DA{_ruleId++:D2}",
+                id: $"DLC{_ruleId++:D2}",
                 title: title,
                 messageFormat: msg,
                 category: "Dealloc",
-                defaultSeverity: DiagnosticSeverity.Error,
+                defaultSeverity: severity,
                 isEnabledByDefault: true
             );
         }
 
         private static readonly DiagnosticDescriptor FieldRule = Rule(
             "Field of Non-Dealloc Type",
-            "Structure `{0}` can be a field only of a structure marked with `DeallocApi`"
+            "Structure `{0}` can be a field only of a structure marked with `DeallocApi`",
+            DiagnosticSeverity.Error
+        );
+
+        private static readonly DiagnosticDescriptor RedundantRule = Rule(
+            "Redundant DeallocApi Attribute",
+            "Structure `{0}` is marked with `DeallocApi` attribute but doesn't have any fields to deallocate",
+            DiagnosticSeverity.Warning
         );
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(
-            FieldRule
+            FieldRule,
+            RedundantRule
         );
 
         public override void Initialize(AnalysisContext context)
@@ -39,6 +49,7 @@ namespace DnDev.Roslyn
             context.EnableConcurrentExecution();
 
             context.RegisterSymbolAction(AnalyzeField, SymbolKind.Field);
+            context.RegisterSyntaxNodeAction(AnalyzeStruct, SyntaxKind.StructDeclaration);
         }
 
         private static void AnalyzeField(SymbolAnalysisContext ctx)
@@ -51,10 +62,21 @@ namespace DnDev.Roslyn
                 return;
 
             if (IsDeallocType(sym.Type) || IsUnmanagedRefList(sym.Type))
-            {
                 ctx.ReportDiagnostic(Diagnostic.Create(FieldRule, sym.Locations.First(), sym.Type.Name));
-            }
         }
+
+        private static void AnalyzeStruct(SyntaxNodeAnalysisContext ctx)
+        {
+            var sym = ctx.SemanticModel.GetDeclaredSymbol((StructDeclarationSyntax)ctx.Node);
+            if (sym == null || !IsDeallocType(sym))
+                return;
+
+            if (sym.GetMembers().Where(m => m.Kind == SymbolKind.Field).Cast<IFieldSymbol>().Any(field => IsDeallocType(field.Type) || IsUnmanagedRefList(field.Type)))
+                return;
+
+            ctx.ReportDiagnostic(Diagnostic.Create(RedundantRule, sym.Locations.First(), sym.Name));
+        }
+
 
         private static bool IsDeallocType(ITypeSymbol type)
         {
