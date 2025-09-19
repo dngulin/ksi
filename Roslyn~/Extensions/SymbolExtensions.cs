b4f6f418a402eb25929ff3119ff69a227af6db08
Danil Gulin <d.nk.gulin@gmail.com>
2025-09-20 00:03:30 +0200

    Cleanup code

--- Roslyn~/Extensions/SymbolExtensions.cs
+++ Roslyn~/Extensions/SymbolExtensions.cs
@@ -1,15 +1,14 @@
 using Microsoft.CodeAnalysis;
 using static Ksi.Roslyn.SymbolNames;
 
-namespace Ksi.Roslyn.Extensions
-{
-    public static class SymbolExtensions
-    {
-        public static bool IsRefOrWrappedRef(this ILocalSymbol self) => self.IsRef || self.Type.IsWrappedRef();
+namespace Ksi.Roslyn.Extensions;
 
-        public static bool Is(this AttributeData attribute, string attributeName)
-        {
-            return attribute.AttributeClass != null && attribute.AttributeClass.Name == attributeName + Suffix;
-        }
+public static class SymbolExtensions
+{
+    public static bool IsRefOrWrappedRef(this ILocalSymbol self) => self.IsRef || self.Type.IsWrappedRef();
+
+    public static bool Is(this AttributeData attribute, string attributeName)
+    {
+        return attribute.AttributeClass != null && attribute.AttributeClass.Name == attributeName + Suffix;
     }
 }
