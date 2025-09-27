74760b9b3d9549f8241041a8bd035caa5d78240c
Danil Gulin <d.nk.gulin@gmail.com>
2025-09-27 12:56:49 +0200

    Add unit tests project

---
+++ Roslyn~/Ksi.Roslyn.Tests/ExplicitCopyTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Testing;
+
+namespace Ksi.Roslyn.Tests;
+
+public class ExplicitCopyTests
+{
+    [Fact]
+    public async Task ExpCopy01IsTriggeredByMethodArg()
+    {
+        var test = new CSharpAnalyzerTest<ExplicitCopyAnalyzer, DefaultVerifier>
+        {
+            ReferenceAssemblies = ReferenceAssemblies.NetStandard.NetStandard21,
+            TestState =
+            {
+                AdditionalReferences = { typeof(RefList<>).Assembly }
+            },
+            TestCode =
+                """
+                using Ksi;
+                
+                [ExplicitCopy]
+                public struct TestStruct
+                {
+                    public int Field;
+                }
+                
+                public static class Test
+                {
+                    public static void Method(TestStruct {|EXPCOPY01:arg|}) {}
+                }
+                """
+        };
+
+        await test.RunAsync();
+    }
+}
