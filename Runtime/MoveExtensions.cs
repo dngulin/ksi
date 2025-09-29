8d96d2379f18ba80da5e9c584542ee6595573af5
Danil Gulin <d.nk.gulin@gmail.com>
2025-09-29 23:44:49 +0200

    Fix SuppressMessage id in MoveExtensions

--- Runtime/MoveExtensions.cs
+++ Runtime/MoveExtensions.cs
@@ -4,7 +4,7 @@ namespace Ksi
 {
     public static class MoveExtensions
     {
-        [SuppressMessage("ExplicitCopy", "EXPCOPY07:Copied by Assignment")]
+        [SuppressMessage("ExplicitCopy", "EXPCOPY04:Assignment copy of the [ExplicitCopy] instance")]
         [SuppressMessage("Dealloc", "DEALLOC04:Dealloc Instance Overwrite")]
         public static T Move<[ExplicitCopy, Dealloc] T>(this ref T self) where T : struct
         {
