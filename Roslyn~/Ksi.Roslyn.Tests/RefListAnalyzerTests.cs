59d19b01b3ddc524b3e8572e28dc95beb7da2cb9
Danil Gulin <d.nk.gulin@gmail.com>
2025-10-01 23:05:42 +0200

    Add unit test for REFLIST03

--- Roslyn~/Ksi.Roslyn.Tests/RefListAnalyzerTests.cs
+++ Roslyn~/Ksi.Roslyn.Tests/RefListAnalyzerTests.cs
@@ -59,4 +59,39 @@ public class RefListAnalyzerTests
             """
         );
     }
+
+    [Fact]
+    public async Task RefList03NonSpecializedCall()
+    {
+        await RefListAnalyzerTest.RunAsync(
+            // language=cs
+            """
+            using Ksi;
+            
+            [ExplicitCopy, DynSized, Dealloc]
+            public struct TestStruct
+            {
+                public RefList<int> List;
+            }
+
+            public static class Test
+            {
+                public static void Method()
+                {
+                    var a = RefList.Empty<TestStruct>();
+                    var b = RefList.Empty<TestStruct>();
+                    
+                    // Use generic non-specialized API:
+                    {|REFLIST03:a.CopyTo<TestStruct>(ref b)|};
+                    {|REFLIST03:a.CopyFrom<TestStruct>(b)|};
+                    
+                    {|REFLIST03:a.Dealloc<TestStruct>()|};
+                    {|REFLIST03:a.Deallocated<TestStruct>()|} = default;
+                    {|REFLIST03:a.Clear<TestStruct>()|};
+                    {|REFLIST03:a.RemoveAt<TestStruct>(0)|};
+                }
+            }
+            """
+        );
+    }
 }
