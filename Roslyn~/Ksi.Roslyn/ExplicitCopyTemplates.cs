694a13dae8a76a32fe58c99c3812e7c01f7abedf
Danil Gulin <d.nk.gulin@gmail.com>
2025-10-05 23:34:10 +0200

    Fix indents in ExplicitCopyTemplates

--- Roslyn~/Ksi.Roslyn/ExplicitCopyTemplates.cs
+++ Roslyn~/Ksi.Roslyn/ExplicitCopyTemplates.cs
@@ -36,14 +36,14 @@ namespace Ksi.Roslyn
             /// </summary>
             public static void CopyFrom(this ref {0}<{1}> self, in {0}<{1}> other)
             {{
-               while (self.Count() > other.Count())
-                   self.RemoveAt(self.Count() - 1);
-
-               if (self.Count() < other.Count())
-                   self.AppendDefault(other.Count() - self.Count());
-               
-               for (var i = 0; i < other.Count(); i++)
-                   self.RefAt(i).CopyFrom(other.RefReadonlyAt(i));
+                while (self.Count() > other.Count())
+                    self.RemoveAt(self.Count() - 1);
+                
+                if (self.Count() < other.Count())
+                    self.AppendDefault(other.Count() - self.Count());
+                
+                for (var i = 0; i < other.Count(); i++)
+                    self.RefAt(i).CopyFrom(other.RefReadonlyAt(i));
             }}
             
             /// <summary>
@@ -51,7 +51,7 @@ namespace Ksi.Roslyn
             /// </summary>
             public static void CopyTo(this in {0}<{1}> self, ref {0}<{1}> other)
             {{
-               other.CopyFrom(self);
+                other.CopyFrom(self);
             }}
             """;
     }
