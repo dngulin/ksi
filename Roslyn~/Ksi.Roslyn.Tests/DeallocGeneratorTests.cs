d715f527cf07f655bc5383d564d0dca1e8cde92d
Danil Gulin <d.nk.gulin@gmail.com>
2025-10-06 18:22:42 +0200

    Add `DeallocGenerator` base unit tests

--- Roslyn~/Ksi.Roslyn.Tests/DeallocGeneratorTests.cs
+++ Roslyn~/Ksi.Roslyn.Tests/DeallocGeneratorTests.cs
@@ -51,4 +51,207 @@ public class DeallocGeneratorTests
             """
         );
     }
+
+    [Fact]
+    public async Task OnlyManagedRefListExtensions()
+    {
+        await DeallocGeneratorTest.RunAsync(
+            // language=cs
+            """
+            using Ksi;
+
+            [ExplicitCopy, DynSized, Dealloc]
+            public struct MyStruct 
+            {
+                public int Primitive;
+                public RefList<int> Collection;
+                public ManagedRefList<int> Collection2;
+            }
+            """,
+            "MyStruct.Dealloc.g.cs",
+            // language=cs
+            """
+            using Ksi;
+
+            public static class MyStruct_Dealloc
+            {
+                public static void Dealloc(this ref MyStruct self)
+                {
+                    self.Collection.Dealloc();
+                }
+
+            """ +
+            DeallocatedExtension.IndentFormat(1, "MyStruct") +
+            RefListDeallocOnlyItems.IndentFormat(1, "ManagedRefList", "MyStruct") +
+            RefListDeallocated.IndentFormat(1, "ManagedRefList", "MyStruct") +
+            RefListSpecialized.IndentFormat(1, "ManagedRefList", "MyStruct") +
+            """
+            }
+
+            """
+        );
+    }
+
+    [Fact]
+    public async Task OnlyTempRefListExtensions()
+    {
+        await DeallocGeneratorTest.RunAsync(
+            // language=cs
+            """
+            using Ksi;
+
+            [ExplicitCopy, DynSized, Dealloc, TempAlloc]
+            public struct MyStruct 
+            {
+                public int Primitive;
+                public RefList<int> Collection;
+                public TempRefList<int> Collection2;
+            }
+            """,
+            "MyStruct.Dealloc.g.cs",
+            // language=cs
+            """
+            using Ksi;
+
+            public static class MyStruct_Dealloc
+            {
+                public static void Dealloc(this ref MyStruct self)
+                {
+                    self.Collection.Dealloc();
+                }
+
+            """ +
+            DeallocatedExtension.IndentFormat(1, "MyStruct") +
+            RefListDeallocOnlyItems.IndentFormat(1, "TempRefList", "MyStruct") +
+            RefListDeallocated.IndentFormat(1, "TempRefList", "MyStruct") +
+            RefListSpecialized.IndentFormat(1, "TempRefList", "MyStruct") +
+            """
+            }
+
+            """
+        );
+    }
+
+    [Fact]
+    public async Task NoRefListExtensions()
+    {
+        await DeallocGeneratorTest.RunAsync(
+            // language=cs
+            """
+            using Ksi;
+
+            [ExplicitCopy, DynSized, Dealloc, TempAlloc]
+            public struct MyStruct 
+            {
+                public int Primitive;
+                public RefList<int> Collection;
+                public TempRefList<int> Collection2;
+                public ManagedRefList<int> Collection3;
+            }
+            """,
+            "MyStruct.Dealloc.g.cs",
+            // language=cs
+            """
+            using Ksi;
+
+            public static class MyStruct_Dealloc
+            {
+                public static void Dealloc(this ref MyStruct self)
+                {
+                    self.Collection.Dealloc();
+                }
+
+            """ +
+            DeallocatedExtension.IndentFormat(1, "MyStruct") +
+            """
+            }
+
+            """
+        );
+    }
+
+    [Fact]
+    public async Task NamespaceIsPickedUp()
+    {
+        await DeallocGeneratorTest.RunAsync(
+            // language=cs
+            """
+            using Ksi;
+
+            namespace Test.Ns
+            {
+                [ExplicitCopy, DynSized, Dealloc, TempAlloc]
+                public struct MyStruct 
+                {
+                    public int Primitive;
+                    public RefList<int> Collection;
+                    public TempRefList<int> Collection2;
+                    public ManagedRefList<int> Collection3;
+                }
+            }
+            """,
+            "MyStruct.Dealloc.g.cs",
+            // language=cs
+            """
+            using Ksi;
+
+            namespace Test.Ns
+            {
+                public static class MyStruct_Dealloc
+                {
+                    public static void Dealloc(this ref MyStruct self)
+                    {
+                        self.Collection.Dealloc();
+                    }
+
+            """ +
+            DeallocatedExtension.IndentFormat(2, "MyStruct") +
+            """
+                }
+            }
+
+            """
+        );
+    }
+
+    [Fact]
+    public async Task EnclosingTypeNameIsPickedUp()
+    {
+        await DeallocGeneratorTest.RunAsync(
+            // language=cs
+            """
+            using Ksi;
+
+            public static class EnclosingType
+            {
+                [ExplicitCopy, DynSized, Dealloc, TempAlloc]
+                public struct MyStruct 
+                {
+                    public int Primitive;
+                    public RefList<int> Collection;
+                    public TempRefList<int> Collection2;
+                    public ManagedRefList<int> Collection3;
+                }
+            }
+            """,
+            "EnclosingType.MyStruct.Dealloc.g.cs",
+            // language=cs
+            """
+            using Ksi;
+            
+            public static class EnclosingType_MyStruct_Dealloc
+            {
+                public static void Dealloc(this ref EnclosingType.MyStruct self)
+                {
+                    self.Collection.Dealloc();
+                }
+
+            """ +
+            DeallocatedExtension.IndentFormat(1, "EnclosingType.MyStruct") +
+            """
+            }
+
+            """
+        );
+    }
 }
