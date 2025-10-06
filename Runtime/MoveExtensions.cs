5feb60c3ede5d6d2e6d6c0b43551a69d3ab89d90
Danil Gulin <d.nk.gulin@gmail.com>
2025-10-06 18:27:52 +0200

    Use pragma insted of the `SuppressMessage` extension

--- Runtime/MoveExtensions.cs
+++ Runtime/MoveExtensions.cs
@@ -1,15 +1,15 @@
-using System.Diagnostics.CodeAnalysis;
-
 namespace Ksi
 {
     public static class MoveExtensions
     {
-        [SuppressMessage("ExplicitCopy", "EXPCOPY04:Assignment copy of the [ExplicitCopy] instance")]
-        [SuppressMessage("Dealloc", "DEALLOC04:Dealloc Instance Overwrite")]
         public static T Move<[ExplicitCopy, Dealloc] T>(this ref T self) where T : struct
         {
+#pragma warning disable EXPCOPY04
             var moved = self;
+#pragma warning restore EXPCOPY04
+#pragma warning disable DEALLOC04
             self = default;
+#pragma warning restore DEALLOC04
             return moved;
         }
     }
