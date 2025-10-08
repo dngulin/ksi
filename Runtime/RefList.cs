60a30df261887614e3a73e782d9577ddb9426ceb
Danil Gulin <d.nk.gulin@gmail.com>
2025-10-08 23:45:06 +0200

    Add docstings for [RefList] types

--- Runtime/RefList.cs
+++ Runtime/RefList.cs
@@ -3,6 +3,10 @@
 
 namespace Ksi
 {
+    /// <summary>
+    /// Dynamic array collection wrapping the Persistent allocator.
+    /// Requires manual deallocation.
+    /// </summary>
     [ExplicitCopy, DynSized, Dealloc, RefList]
     public struct RefList<T> where T : unmanaged
     {
@@ -10,6 +14,10 @@ namespace Ksi
         internal int Count;
     }
 
+    /// <summary>
+    /// Dynamic array collection wrapping the Temp allocator.
+    /// Can be stored only on stack.
+    /// </summary>
     [ExplicitCopy, DynSized, TempAlloc, RefList]
     public struct TempRefList<T> where T : unmanaged
     {
@@ -17,6 +25,10 @@ namespace Ksi
         internal int Count;
     }
 
+    /// <summary>
+    /// Dynamic array collection wrapping the Managed allocator.
+    /// Can store structures containing reference types, but it is not compatible with Burst.
+    /// </summary>
     [ExplicitCopy, DynSized, RefList]
     public struct ManagedRefList<T> where T : struct
     {
