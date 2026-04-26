99cafedd6bda0b1d93e8b2ae8bf48eb9f6d7c904
Danil Gulin <d.nk.gulin@gmail.com>
2026-04-26 15:20:00 +0200

    Add extension method to represent LenPrefixSize in bytes

--- Runtime/Serialization/ValueQualifier.LenPrefixSize.cs
+++ Runtime/Serialization/ValueQualifier.LenPrefixSize.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Ksi.Serialization
 {
     /// <summary>
@@ -24,5 +26,29 @@ namespace Ksi.Serialization
         /// 32-bit length prefix (4 bytes).
         /// </summary>
         _32,
+    }
+
+    /// <summary>
+    /// Provides extension methods for <see cref="LenPrefixSize"/>.
+    /// </summary>
+    public static class LenPrefixSizeExtensions
+    {
+        /// <summary>
+        /// Gets the size of the length prefix in bytes.
+        /// </summary>
+        /// <param name="size">The <see cref="LenPrefixSize"/>.</param>
+        /// <returns>The size in bytes.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when an invalid <see cref="LenPrefixSize"/> is provided.</exception>
+        public static int InBytes(this LenPrefixSize size)
+        {
+            return size switch
+            {
+                LenPrefixSize._0 => 0,
+                LenPrefixSize._8 => 1,
+                LenPrefixSize._16 => 2,
+                LenPrefixSize._32 => 4,
+                _ => throw new ArgumentOutOfRangeException(nameof(size), size, null)
+            };
+        }
     }
 }
